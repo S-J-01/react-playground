44468f3aa3fc4bd034114d5c752681374a5e8709
Shreyas Jaltare <shreyasjaltare@gmail.com>
2024-04-18 23:51:57 +0530

    initial components and props

--- frontend-todo-app/src/App.jsx
+++ frontend-todo-app/src/App.jsx
@@ -5,28 +5,39 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const[todoForToday, setTodoForToday] = useState({
+  const[todos, setTodos] = useState([{
     title: "buy ps5",
     description: "but ps5 on amazon",
     id:1
-  })
+  },
+  {
+    title: "buy pepsi",
+    description: "but pepsi on amazon",
+    id:1
+  }])
 
-  setInterval(() => {
-    setTodoForToday({
-      title:"buy ps5 updated" + Math.random(),
-      description:"buy ps5 on amazon updated" + Math.random(),
-      id:1
-    })
-  }, 5000);
+  // setInterval(() => {
+  //   setTodoForToday({
+  //     title:"buy ps5 updated" + Math.random(),
+  //     description:"buy ps5 on amazon updated" + Math.random(),
+  //     id:1
+  //   })
+  // }, 5000);
   return (
    <div>
-    {todoForToday.title}
-    <br />
-    {todoForToday.description}
+    {todos.map((item)=>{
+      return(<Todo title={item.title} description={item.description}></Todo>)
+    })}
    </div>
   )
 }
 
+function Todo(props){
+  return (<div>
+    {props.title} &nbsp;
+    {props.description}
+  </div>)
+}
 
 
 export default App
