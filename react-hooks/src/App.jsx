e41c217139f7b5573cb16ac34858b594ffbbb2c0
Shreyas Jaltare <shreyasjaltare@gmail.com>
2026-04-10 21:00:52 +0530

    add react-hooks folder

---
+++ react-hooks/src/App.jsx
@@ -0,0 +1,37 @@
+import { useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const focusRef = useRef(null);
+
+  function focusOnEmail() {
+    focusRef.current.focus();
+  }
+
+  return (
+    <>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <input
+          type={"text"}
+          placeholder={"enter email"}
+          style={{ margin: 10, padding: 20 }}
+          ref={focusRef}
+        ></input>
+        <input
+          type={"text"}
+          placeholder={"enter password"}
+          style={{ margin: 10, padding: 20 }}
+        ></input>
+        <button style={{ margin: 10, padding: 20 }} onClick={focusOnEmail}>
+          Submit
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
